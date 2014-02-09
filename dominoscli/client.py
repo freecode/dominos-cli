f8f8acd382719205606c9e0571e1dee0717f4eef
Rick van Biljouw <rb10054603@gmail.com>
2014-02-09 02:04:17 +0000

    Initial commit

--- dominoscli/client.py
+++ dominoscli/client.py
@@ -2,10 +2,10 @@ from suds.client import Client
 
 
 class DominosClient:
+    """ Simple SOAP client for dominos """
 
     def __init__(self, live=False):
         if not live:
             self.client = Client("http://testweb.dominos.co.uk/dominos.services/PizzaService.asmx?WSDL")
         else:
             self.client = Client("http://dominos.co.uk/dominos.services/PizzaService.asmx?WSDL")
-        print self.client
